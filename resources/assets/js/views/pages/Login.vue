5fbc800272a931d99197969bcfb8017ba9ef85c7
Gabriel Galan Mendez <gabrielgalanmendez@MacBook-Air-de-Gabriel-2.local>
2017-10-14 12:18:15 -0500

    remove create user

--- resources/assets/js/views/pages/Login.vue
+++ resources/assets/js/views/pages/Login.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="app flex-row align-items-center">
-    <div class="container">
-      <router-link :to="{ name: 'Register' }">Crear Cuenta</router-link>
-      
+    <div class="container">      
       <div class="row justify-content-center">
         <div class="col-md-8">
           <div class="card-group mb-0">
