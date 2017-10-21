581c645d12d783e1b11e40437b135dade35e286f
Gabriel Galan Mendez <gabrielgalanmendez@MacBook-Air-de-Gabriel-2.local>
2017-10-21 10:33:11 -0500

    change rute folder

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -26,7 +26,7 @@ class ClientesController extends Controller
         $image_type_aux = explode("image/", $image_parts[0]);
         $image_type = $image_type_aux[1];
         $image_base64 = base64_decode($image_parts[1]);
-        $file = '../../'.UPLOAD_DIR . uniqid() . '.png';
+        $file = '../'.UPLOAD_DIR . uniqid() . '.png';
         file_put_contents($file, $image_base64);
 
         $cliente = new Cliente;
