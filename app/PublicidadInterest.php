ebb4bc34f4e9a817e6fb22aedda5fef8f729eb45
g4br13lg4l4n <gabriel.galan92@gmail.com>
2017-10-26 13:38:14 -0500

    delete migrations

--- app/PublicidadInterest.php
+++ app/PublicidadInterest.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SocialProfiles extends Model
+class PublicidadInterest extends Model
 {
     //
 }
---
+++ database/migrations/2017_10_25_231931_create_user_aplications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_aplications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('name');
+            $table->string('age');
+            $table->string('gender');
+            $table->string('locations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_aplications');
+    }
+}
